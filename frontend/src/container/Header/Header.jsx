ae3114993da3058cb7a6886b93f3cf4ef1c1dcf1
Russell Feng <z5405532@ad.unsw.edu.au>
2023-07-29 20:47:36 +1000

    modified header

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -3,9 +3,20 @@ import'./Header.scss';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
 
+const scaleVariants = {
+  whileInView : {
+    scale: [0,1],
+    opacity: [0,1],
+    transition:{
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div id='home' className='app__header app__flex'>
       <motion.div
         whileInView={{ x: [-100,0 ], opacity: [0, 1] }}
         transition={{ duration: 1.5 }}
@@ -19,8 +30,41 @@ const Header = () => {
               <h1 className='head-text'>Russell Feng</h1>
             </div>
           </div>
+
+          <div className='tag-cmp app__flex'>
+            <p className="p-text">Web developer</p>
+            <p className="p-text">Frontend developer</p>
+          </div>
         </div>
       </motion.div>
+
+      <motion.div 
+        whileInView={{ x: [-100,0 ], opacity: [0, 1] }}
+        transition={{ duration: 1.5, delaychildren : 0.5}}
+        className='app__header-img'>
+        <img src={images.profile} alt="profile_bg" />
+        <motion.img
+          whileInView={{ x: [-100,0 ], scale: [0, 1] }}
+          transition={{ duration: 2.5, ease: 'easeInOut'}}
+          src={images.circle} alt="profile_circle" 
+          className='overlay_circle'>
+        </motion.img>
+      </motion.div>
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className='app__header_circles'
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className='circle_cmp app__flex' key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+
+          </div>
+        ))}
+      </motion.div>
+
+      
 
     </div>
   )
