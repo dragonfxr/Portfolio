447b28984d5845103270d6a4b60c68ca99843ec7
Russell Feng <z5405532@ad.unsw.edu.au>
2023-10-01 01:23:52 +1000

    started skills section

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState }from 'react';
+import { motion } from 'framer-motion';
+import ReactToolTip from 'react-tooltip';// new, need installation
+import { AppWrap } from '../../wrapper';
+import { urlFor, client} from '../../client';
+
 import'./Skills.scss';
 
 
 const Skills = () => {
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([])
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setExperience(data);
+    })
+
+    client.fetch(skillsQuery)
+      .then((data) => {
+        setSkills(data);
+    })
+  }, [])
+
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='head-text'>Skills & Experience</h2>
+      <div className='app__skills-container'>
+        <motion.div
+          className='app__skills-list'
+        >
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{opacity:[0,1]}}
+              transition={{duration:0.5}}
+              className='app__skills-item app__flex'
+              key={skill.name}
+            >
+              <div className='app__flex' style={{backgroundColor: skill.bgColor}}>
+                <img src={urlFor(skill.icon)} alt={skill.name}/>
+                <p className='p-text'>{skill.name}</p>
+              </div>
+            </motion.div>
+          ))}
+        </motion.div>
+
+      </div>
+    </>
   )
 }
 
--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState }from 'react';
 import {AiFillGithub, AiFillEye} from 'react-icons/ai';
-import { animate, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { AppWrap } from '../../wrapper';
 import { urlFor, client} from '../../client';
 
